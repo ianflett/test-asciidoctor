6d5dd5de4da848fe8602d067562e0e119d25d162
ianflett <ian.flett@gmail.com>
2019-06-09 23:24:53 +0100

    Initial commit.
    Tries to create a book with Asciidoctor outputting to HTML5, PDF and EPUB using Asciidoctor diagram and AsciiMath.

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package example;
+
+public class HelloWorld {
+
+    // tag::main[]
+
+    public static void main(String ... args) {
+
+        System.out.println("Hello World!");
+    }
+
+    // end::main[]
+}
